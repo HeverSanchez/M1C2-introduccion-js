eba2dcf8be66358c8bb16f4874adef8fa33f951b
HeverSanchez <argonhever@gmail.com>
2023-09-02 23:41:05 -0300

    aprendiendo do-While

---
+++ 1 - JavaScript Fundamentos/Ejercicio 03.js
@@ -0,0 +1,83 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*3️⃣ EJERCICIO 03 3️⃣*/
+
+function sonIguales(x, y) {
+   // Retorna true si "x" e "y" son iguales.
+   // De lo contrario, retorna false.
+   // Tu código:
+   if (x === y){
+      return true;
+   } else {
+      return false;
+   }
+}
+
+function tienenMismaLongitud(str1, str2) {
+   // Retorna true si los dos strings tienen la misma longitud.
+   // De lo contrario, retorna false.
+   // Tu código:
+   if (str1.length === str2.length){
+      return(true);
+   } else {
+      return(false);
+   }
+}
+console.log(tienenMismaLongitud("hola", "adios"));
+
+function menosQueNoventa(num) {
+   // Retorna true si el argumento "num" es menor que noventa.
+   // De lo contrario, retorna false.
+   // Tu código:
+   if (num < 90){
+      return true;
+   } else {
+      return false;
+   }
+}
+console.log(menosQueNoventa(500));
+
+function mayorQueCincuenta(num) {
+   // Retorna true si el argumento "num" es mayor que cincuenta.
+   // De lo contrario, retorna false.
+   // Tu código:
+   if (num > 50){
+      return true;
+   } else {
+      return false
+   }
+}
+console.log(mayorQueCincuenta(55));
+
+function esPar(num) {
+   // Retorna true si "num" es par.
+   // De lo contrario, retorna false.
+   // Tu código:
+   if (num % 2 === 0){
+      return true;
+   } else {
+      return false;
+   }
+}
+console.log(esPar(5));
+
+function esImpar(num) {
+   // Retorna true si "num" es impar.
+   // De lo contrario, retorna false.
+   // Tu código:
+   if (num % 2 !== 0){
+      return true;
+   } else {
+      return false;
+   }
+}
+console.log(esImpar(5));
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   tienenMismaLongitud,
+   sonIguales,
+   menosQueNoventa,
+   mayorQueCincuenta,
+   esPar,
+   esImpar,
+};
